3df21a226bb6e3cb4e7e9802aa6922e47ffdf427
Minh Quy <dominhquy@gmail.com>
2017-04-05 00:07:24 +0700

    Add source code. Update README.md and .gitignore file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ShipAZ
+
+ShipAZ application that I made in 2015: only layout
---
+++ src/com/shipaz/MainActivity.java
@@ -0,0 +1,19 @@
+package com.shipaz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 
+ * @author quydm
+ *
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.shipaz_homevs2hd);
+	}
+
+}
